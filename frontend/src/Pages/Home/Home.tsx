bb117687660607f29d4242c04803da8fdc01b39e
Emiliahrabeson <emiliahrabeson@gmail.com>
2026-07-28 22:09:58 +0300

    feat: get statistics

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,7 +1,39 @@
 import "./home.css";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  // const url1 = "http://localhost:3000/api/salles/nb_salle"
+  const [nb_salle_dispo, setNb_salle_dispo] = useState(0);
+  const [reservations_aujourdhui, setReservations_aujourdhui] = useState(0);
+  const [demandes_en_attente, setdemandes_en_attente] = useState(0);
+  const [taux_occupation, setTaux_occupation] = useState(0);
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      const url = "http://localhost:3000/api/salles/stats";
+      try {
+        const res = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+
+        const data = await res.json();
+        console.log("res.stats:", data);
+
+        setNb_salle_dispo(data.salles_disponibles);
+        setReservations_aujourdhui(data.reservations_aujourdhui);
+        setdemandes_en_attente(data.demandes_en_attente);
+        setTaux_occupation(data.taux_occupation);
+      } catch (err) {
+        console.error("Erreur du chargement des stats:", err);
+      }
+    };
+
+    fetchStats();
+  }, []);
+
   return (
     <>
       <div className="title_bar">
@@ -14,15 +46,19 @@ const Home = () => {
       <div className="statistique">
         <div className="division">
           <p>Salles disponibles</p>
+          <p>{nb_salle_dispo}</p>
         </div>
         <div className="division">
           <p>Réservation aujourd'hui</p>
+          <p>{reservations_aujourdhui}</p>
         </div>
         <div className="division">
           <p>demande en attente</p>
+          <p>{demandes_en_attente}</p>
         </div>
         <div className="division">
           <p>Taux d'occupation</p>
+          <p>{taux_occupation}%</p>
         </div>
       </div>
 
